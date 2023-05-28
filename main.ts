789625da0033ad1f1825e235f2b25fc037f1413e
Garragames <96148865+garragames@users.noreply.github.com>
2023-05-28 09:06:18 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
 
+
+
+namespace myFieldEditor {
+    // Este es tu nuevo bloque
+    //% block="my block $myArg"
+    //% myArg.fieldEditor="text"
+    export function myBlock(myArg: string) {
+        // Tu código aquí
+    }
+
+}
